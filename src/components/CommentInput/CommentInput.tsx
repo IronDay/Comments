1d5c27d22be7a97c5c8a5f7fe1fd7e508989e2ce
Djioleu Florian <nsiegningflorian@gmail.com>
2023-08-14 12:32:06 +0100

    Refactoring: better comment handling support

--- src/components/CommentInput/CommentInput.tsx
+++ src/components/CommentInput/CommentInput.tsx
@@ -1,20 +1,23 @@
 import styles from './CommentInput.module.css';
 import profile from "../../assets/profile.jpg";
+import React from "react";
 
 interface Props {
     image: string,
     onSubmit: (message: string) => void
 }
 
-const CommentInput = ({image, onSubmit}: Props) => {
+const CommentInput = ({onSubmit}: Props) => {
+    const handleCommentSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        const target = e.currentTarget
+        if (e.key === "Enter" && target?.value.length !== 0) {
+            onSubmit(target?.value)
+        }
+    }
     return (
         <div className={[styles["comments__input"], styles["grid"], styles["grid--1x2"]].join(" ")}>
             <div className={styles["comments__profile"]}><img src={profile} alt=""/></div>
-            <input className={styles.input} onKeyPress={(e) => {
-                if (e.key === "Enter" && e.target.value.length !== 0) {
-                    onSubmit(e.target.value)
-                }
-            }} type="text"
+            <input className={styles.input} onKeyPress={handleCommentSubmit} type="text"
                    placeholder="Start the discussion"/>
         </div>
     )
